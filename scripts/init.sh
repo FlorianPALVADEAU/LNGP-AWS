e2cef80d49ea3cb4e61e69d656ed536dd310e6cb
Louis Perrenot <louis.perrenot@edu.devinci.net>
2024-06-05 21:50:13 +0200

    Fix appspec

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,9 +2,6 @@
 
 sudo docker-compose up -d --build --force-recreate --remove-orphans
 echo 'Docker build'
-while ! sudo docker-compose ps | grep "qlowerBackend" | grep -q "Up"; do
-  sleep 5
-done
 
 echo 'sudo docker image prune -f'
 sudo docker image prune -f
