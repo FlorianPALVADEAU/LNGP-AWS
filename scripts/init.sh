34e545f17b9f0265b334b46507693660bf41c772
Louis Perrenot <louis.perrenot@edu.devinci.net>
2024-06-05 18:16:28 +0200

    Add docker

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+sudo docker-compose up -d --build --force-recreate --remove-orphans
+echo 'Docker build'
+while ! sudo docker-compose ps | grep "qlowerBackend" | grep -q "Up"; do
+  sleep 5
+done
 
-npm install && npm run build
+echo 'sudo docker image prune -f'
+sudo docker image prune -f
+
+echo 'sudo docker ps'
+sudo docker ps
+
+sudo systemctl start nginx
